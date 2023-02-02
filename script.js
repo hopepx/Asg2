efa25da8556c23b53c743fe0f52ccd46c3921536
hopepx <hopeparadoxsolos@gmail.com>
2023-02-03 00:34:07 +0800

    [Add] Bestsellers Section - [✓] Added About, Contact HTML & Featured CSS - [✓] Added Bestsellers Section - [✓] Did Some Javascript and Styling for Bestsellers Section - [X] Didn't manage to fix all the bugs

---
+++ script.js
@@ -0,0 +1,28 @@
+const carousel = new Swiper('.carouselbox', {
+	spaceBetween: 30, 
+	slidesPerView: 'auto', 
+	centeredSlides: true,
+	
+	// If we need pagination 
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevtEl: '.swiper-button-prev',
+	},
+    breakpoints: {
+	    481: {
+		    slidesPerView: 2,
+		    slidesPerGroup: 1,
+		    centeredSlides: false,
+	},
+	    640: {
+		    slidesPerView: 3,
+		    slidesPerGroup: 3, 
+            centeredSlides: false,
+	},
+	    992: {
+		    slidesPerView: 4,
+		    slidesPerGroup: 4,
+		    centeredSlides: false,
+	}
+}
+});
